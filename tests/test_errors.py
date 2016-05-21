57802d7931c3dec5b7f5723131b9b81c028cfe5b
Cesar Canassa <cesar.canassa@gmail.com>
2016-05-21 16:56:58 +0200

    Added more tests

---
+++ tests/test_errors.py
@@ -0,0 +1,9 @@
+import pytest
+
+from falcor import parser
+from falcor import exceptions
+
+
+def test_should_create_a_named_routed_token_for_the_path_and_fail_if_white_space_divides_the_token_name():
+    with pytest.raises(exceptions.Invalid):
+        parser('one[{ranges:f o o}].oneMore', True)
